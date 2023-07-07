13e8d623a2a783d5ea5356b8fb167504f205c046
Nigel Doheny <nigel.doheny93@gmail.com>
2023-07-07 11:14:31 +0000

    Add static css and js

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.views import View
 
 # Create your views here.
+
+
+# Create your views here.
+
+def index(request):
+    # Return homepage
+    return render(request, 'base.html')
