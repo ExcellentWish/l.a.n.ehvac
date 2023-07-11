de3bb284b172cfb52fd2ca0e5cbd90be98a66251
Nigel Doheny <nigel.doheny93@gmail.com>
2023-07-11 14:48:10 +0000

    Add 404 and 500 html pages

--- lanehvac/urls.py
+++ lanehvac/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler400, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'), name='website_urls'),
 ]
+
+handler404 = 'website.views.error_404'
+handler500 = 'website.views.error_500'
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.views import View
+from django.core.mail import send_mail
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -10,3 +12,11 @@ from django.views import View
 def index(request):
     # Return homepage
     return render(request, 'base.html')
+
+
+def error_404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def error_500(request):
+    return render(request, '500.html', status=500)
