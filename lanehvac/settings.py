26e7c2b36c8dd3adef9727a7e98f31df8a939a05
Nigel Doheny <nigel.doheny93@gmail.com>
2023-07-13 11:11:27 +0000

    Amend alloed_hosts

--- lanehvac/settings.py
+++ lanehvac/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
-ALLOWED_HOSTS = ['8000-excellentwish-lanehvac-1wywrgpgbic.ws-eu101.gitpod.io', 'lanehvac-41c8d4a6b4f7.herokuapp.com']
+ALLOWED_HOSTS = ['8000-excellentwish-lanehvac-1wywrgpgbic.ws-eu101.gitpod.io', 'lanehvac-41c8d4a6b4f7.herokuapp.com', 'www.lanehvacmechanical.com' ,'lanehvacmechanical.com']
 
 
 # Application definition
